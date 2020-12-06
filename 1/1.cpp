eeaf02a252824d13bd0df44b9467abf074922637
Sonu Dileep <59635031+SonuDileep@users.noreply.github.com>
2020-12-06 13:50:24 -0500

    Add files via upload

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,131 +1,71 @@
+
+// A Simple program which implements polymorphism, and inheritance
+
 #include<iostream>
 #include<string>
-#include<memory>
-#include<vector>
-#include<typeinfo>
+
 using namespace std;
-// employers name, department 
-class Employee; //forward declaration
-class Employer
-	{
+
+//Stores name, age and weight 
+class Gender
+{
 public:
-	string name;
-	string department;
-	vector<shared_ptr<Employee>> employees_list;
-	shared_ptr<Employee> mm;
-//	vector<Employee> employees_list;
-	vector<string> emp_name;
-	Employer(string n, string dept):name{n}, department{dept}
+	 string name;
+	 int age;
+	 int weight;
+	Gender(string name_, int age_, int weight_):name{name_},age{age_}, weight{weight_}
 	{
-//		cout<<"Employer name :"<<name<<'\n'<<"Department:"<<department<<endl;
-	}
- 	void get_name()
+	 }
+	void get_name()
 	{
-		cout<<"Employer name :"<<name<<'\n'<<"Department:"<<department<<endl;
+	cout<<this->name<<endl;	
 	}
-	void call_share(shared_ptr<Employee> T)
+	void get_age()
 	{
-//		employees_list.pushback(T);
-		cout<<typeid(T).name()<<endl;
-		cout<<typeid(mm).name()<<endl;
+	cout<<this->age<<endl;
 	}
+	void get_weight()
+	{
+	cout<<this->weight<<endl;
+	}
+	virtual void get_gender() //Called from child classes
+	{}
 
-	~Employer()
-	{
-		cout<<"Employer Destroyed"<<endl;
-	}
-	friend class Employee;
-	};
+};
 
-//Employee details
-class Employee : public std::enable_shared_from_this<Employee>
-	{
+// Class which stores name, age and weight of males
+class Male:public Gender
+{
+private:
+	bool temp;
 public:
-	string name;
-	int salary;
-	weak_ptr<Employer> A;
-	Employee(shared_ptr<Employer> temp,string name, int salary):salary{salary},name{name},A{temp}
-	{	shared_ptr<Employer> p1;
-                p1 = A.lock();
-		cout<<(*p1).name<<endl;
-		auto wptr = std::shared_ptr<Employee>( this, [](Employee*){} );
-//		cout<<typeid(shared_from_this()).name<<endl;
-//		shared_ptr<Employee> k = GetSharedPtr();
-//		(*p1).call_share(p2);
-        	(*p1).employees_list.push_back(shared_from_this());
-//		(*temp).emp_name.push_back(name);
+	Male(string name_,int age_, int weight_):Gender(name_, age_, weight_)
+	{temp = true;}
+	void get_gender(){
+	cout<<name<<" is a Male"<<endl;
 	}
-	std::shared_ptr<Employee> GetSharedPtr(){return shared_from_this();}
+};
 
-	void get_info()
-	{
-		shared_ptr<Employer> p;
-                p = A.lock();
-		cout<<"Employer name :"<<(*p).name<<'\n'<<"Employee name:"<<name<<'\n'<<"Department:"<<(*p).department<<'\n'<<"Salary(in USD):"<<salary<<endl;
+// Class which stores name, age and weight of females
+class Female:public Gender
+{
+private:
+	bool temp;
+public:
+	Female(string name_,int age_, int weight_):Gender(name_, age_, weight_)
+	{temp = false;}
+	void get_gender(){
+	cout<<name<<" is a FeMale"<<endl;
 	}
-	friend class Employer;
-	~Employee()
-	{
-		cout<<"Employee Destroyed"<<endl;}
-	};
+};
 
-
-
-int main()
-{       
-// Writing three employers and their department
-	cout<<"Employers..........."<<endl;
-	vector<shared_ptr<Employer>> Employer_list;
-	Employer_list.push_back(move(shared_ptr<Employer>(new Employer("Apple","ECE"))));
-	Employer_list.push_back(move(shared_ptr<Employer>(new Employer("Google","CS"))));
-	Employer_list.push_back(move(shared_ptr<Employer>(new Employer("Sony","ME"))));
-//	Employer_list[1]->get_name();
-	vector<shared_ptr<Employer>>::iterator i;
-	for(i = Employer_list.begin(); i != Employer_list.end(); i++)
-	{
-	(*i)->get_name();
-	}
-// Writing list of employee, their employer
-	vector<shared_ptr<Employee>> Employee_list;
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[0],"Josh",1235)));
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[1],"Mike",8793)));
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[0],"Adrian",9807)));
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[2],"Andrew",3452)));
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[2],"Penny",2354)));
-	Employee_list.push_back(shared_ptr<Employee>(new Employee(Employer_list[0],"Sheldon",1545)));
-        cout<<"Saving Done..........."<<endl;
-	cout<<'\n'<<endl;
-	cout<<'\n'<<endl;
-	cout<<'\n'<<endl;
-	cout<<'\n'<<endl;
-	cout<<"Employees..........."<<endl;
-
-//Print employees name, employer, department and salary
-	vector<shared_ptr<Employee>>::iterator l;
-	for(l = Employee_list.begin();l!=Employee_list.end();l++)
-	{
-	(*l)->get_info();
-	cout<<'\n'<<endl;
-	}
-
-	vector<shared_ptr<Employer>>::iterator j;
-	for(j = Employer_list.begin(); j != Employer_list.end(); j++)
-	{       cout<<'\n'<<endl;
- 		cout<<"Employee:"<<(*j)->name<<endl;
-
-//	vector<string>::iterator m;
-//	for(m = (*j)->emp_name.begin(); m != (*j)->emp_name.end(); m++)
-//	{
-//		cout<<(*m)<<endl;
-//	}
-
-
-	vector<shared_ptr<Employee>>::iterator m;
-	for(m = (*j)->employees_list.begin(); m != (*j)->employees_list.end(); m++)
-	{
-		cout<<(*m)->name<<endl;
-	}
-	}	
+	int main(){
+	Gender *G1,*G2;
+	Male m("Mike",23,160);
+	Female f("Joan",27,139); 
+	G1 = &m;
+	G2 = &f;
+	G1->get_gender();
+	G2->get_gender();
+	return 0;
 }
-
-
