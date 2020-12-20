b5c05acf8cd1f010f44a3c8b1c52b6c3d4c9294e
Sonu Dileep <59635031+SonuDileep@users.noreply.github.com>
2020-12-20 13:25:34 -0500

    Add files via upload

--- 3/3.cpp
+++ 3/3.cpp
@@ -15,19 +15,34 @@ using namespace std;
 ///Case insensitive string comparison function
 int case_insensitive_match(string s1, string s2) 
 {
+   int token_length = s2.size(); 
    //convert s1 and s2 into lower case strings
    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
    if(s1.compare(s2) == 0)
-{
+      {
+ //     cout<<s1<<s2<<endl;
       return 1; //The strings are same
-}
+      }     
    else if (s1.rfind(s2)!= string::npos)
-{
-	cout<<"HEREEEEEEE"<<s1.rfind(s2)<<endl;
-	return 3;
+      {
+	int pos = s1.rfind(s2);
+      if (pos==0)
+      {
+	      char input_char = s1[token_length];
+            if (!((input_char >= 65 && input_char <= 90) || (input_char >= 97 && input_char <= 122)))
+      {	
+	            return 1;
+      }}
+	else
+      {
+            char input_char = s1[0];
+            if (!((input_char >= 65 && input_char <= 90) || (input_char >= 97 && input_char <= 122)))
+      {	
+	            return 1;
+      }}
 }
-	return 3;
+      return 0;
 }
 
       	
@@ -63,19 +78,13 @@ int main(int argc, const char * argv[]) {
      while (readfile >> words) // Read each word in the file
 {
       int x = case_insensitive_match(words, token); // Compares each word to the token
-      cout<<x<<words<<endl;
-      if (x==0) // x will be 0 when the word exactly matches token
+      if (x==1) // x will be 0 when the word exactly matches token
 {
-        count++;
-}
-      if (x==1)
-{
-
-      if (check_string(token, words))
-{
-      
+//      cout<<x<<words<<endl;
       count++;
-}}}}
+}
+
+}}
      cout<<"Frequency of word \""<<token<<"\" = "<<count<<endl;
      return 0;
 }
